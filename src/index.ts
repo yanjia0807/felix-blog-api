b801d0c4defbe7ff5c679bc9e9c3de49921583e0
yanjia <yanjia@hanhua.com>
2025-03-12 20:05:01 +0800

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,19 @@ export default {
    * This gives you an opportunity to extend code.
    */
   register({ strapi }: { strapi: Core.Strapi }) {
-    const socketConfig = strapi.config.get("socket.config") as SocketConfig;
-    if (!socketConfig) {
-      strapi.log.error("Invalid Socket.IO configuration");
-      return;
-    }
-    strapi.server.httpServer.on("listening", () => {
-      // const io = new SocketServer(strapi.server.httpServer, {
-      //   cors: socketConfig.cors,
-      // });
+    // const socketConfig = strapi.config.get("socket.config") as SocketConfig;
+    // if (!socketConfig) {
+    //   strapi.log.error("Invalid Socket.IO configuration");
+    //   return;
+    // }
+    // strapi.server.httpServer.on("listening", () => {
+    //   const io = new SocketServer(strapi.server.httpServer, {
+    //     cors: socketConfig.cors,
+    //   });
 
-      // (strapi as any).io = io;
-      // strapi.eventHub.emit("socket.ready");
-    });
+    //   (strapi as any).io = io;
+    //   strapi.eventHub.emit("socket.ready");
+    // });
   },
 
   /**
@@ -39,9 +39,9 @@ export default {
    * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }: { strapi: Core.Strapi }) {
-    const socketService = strapi.service("api::socket.socket") as {
-      initialize: () => void;
-    };
-    socketService.initialize();
+    // const socketService = strapi.service("api::socket.socket") as {
+    //   initialize: () => void;
+    // };
+    // socketService.initialize();
   },
 };
